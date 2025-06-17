7081afd1c52e0736abb453247f4c59a484d14f6e
Madhesh Medasani <madhesh@Madheshs-MacBook-Air.local>
2025-06-17 11:58:32 +0530

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
 import "./App.scss";
 
 function App() {
-  const name = "Hello THis is Madhesh";
-
-  const name1 = "Hello";
-
-  console.log(name, name1);
-
   return (
     <>
       Initial Template
-      <div>This is the first div</div>
-      <h1>This is test two</h1>
     </>
   );
 }
